b6edda05a8d372ba2a5139006859889c1f9af3be
sdpathak24 <202001258@daiict.ac.in>
2024-01-03 12:20:30 +0530

    list added

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <Navbar />
       <About />
-      <Experience />
+      {/* <Experience />*/}
       <Skills />
       <Achievements />
       <Referals />
--- src/components/skills.js
+++ src/components/skills.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <div>
+      <ul>
+        <li>Web dev</li>
+        <ul>
+          <li>React js</li>
+          <li>Node js</li>
+          <li>Mongo db</li>
+        </ul>
+        <li>cyber sec</li>
+        <li></li>
+      </ul>
+    </div>
+  );
 };
 
 export default Skills;
